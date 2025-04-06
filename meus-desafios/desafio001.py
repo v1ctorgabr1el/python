79b0b5f62f9bc3dbcd14b98393e83f31eea20853
Victor Gabriel <vsilvaxavier72@gmail.com>
2025-04-06 14:07:40 -0300

    fiz o desafio de maior ou menor de idade

---
+++ meus-desafios/desafio001.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print(f"Olá {nome} você é maior de idade!")
+
+else:
+    print(f"Olá {nome} você é menor de idade!")
